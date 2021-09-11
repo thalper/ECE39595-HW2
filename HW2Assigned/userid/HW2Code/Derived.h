b8c45688949a0e17218277d6cf5784f13420d109
Tycho <thalper@purdueedu>
2021-09-11 14:36:42 -0400

    added HW2Assigned

---
+++ HW2Assigned/userid/HW2Code/Derived.h
@@ -0,0 +1,14 @@
+#ifndef DERIVED_H_
+#define DERIVED_H_
+#include "Base.h"
+
+class Derived : public Base {
+public:
+   void f2( );
+   void f3( );
+   virtual void f6( );
+private:
+   void f1( );
+   void f5( );
+};
+#endif /* DERIVED_H_ */
